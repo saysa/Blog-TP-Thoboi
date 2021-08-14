5b4f141213d77f397e484a7be24df68cf478b845
Saysa <sbo@conamed.de>
2021-08-14 23:38:44 +0200

    assert and validator

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
@@ -20,11 +21,15 @@ class Comment
 
     /**
      * @ORM\Column
+     * @Assert\NotBlank
+     * @Assert\Length(min="2")
      */
     private string $author;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(min="5")
      */
     private string $content;
 

a3f6cab03821d79849910b47c4ee6988c4057ffb
Saysa <sbo@conamed.de>
2021-08-31 00:33:03 +0200

    registration

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="registration")
+     */
+    public function __invoke(Request $request, UserPasswordEncoderInterface $userPasswordEncoder): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($userPasswordEncoder->encodePassword($user, $form->get('plainPassword')->getData()));
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
